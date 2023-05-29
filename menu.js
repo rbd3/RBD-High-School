cee84c3c84b45922fa9aec16b03979cbca177ecf
rbd3 <rabedesanaa@gmail.com>
2023-05-29 11:01:41 +0300

    Fix ESLint error

--- menu.js
+++ menu.js
@@ -1,8 +1,5 @@
 const hamburger = document.querySelector('.humburger');
 const closeBtn = document.querySelector('.closebtn');
-const workSection = document.querySelector('.work');
-const aboutSection = document.querySelector('.about-me');
-const contactSection = document.querySelector('.contact-form');
 
 // Show the mobile menu when the hamburger button is clicked
 hamburger.addEventListener('click', () => {
@@ -22,25 +19,5 @@ Portfolio.addEventListener('click', () => {
 const about = document.querySelector('#about');
 about.addEventListener('click', () => {
   aboutSection.scrollIntoView({ behavior: 'smooth' });
-  document.getElementById('myNav').style.width = '0%';
-});
-
-const registration = document.querySelector('#registration');
-contact.addEventListener('click', () => {
-  contactSection.scrollIntoView({ behavior: 'smooth' });
-  document.getElementById('myNav').style.width = '0%';
-});
-
-
-const sponsor = document.querySelector('#sponsor');
-contact.addEventListener('click', () => {
-  contactSection.scrollIntoView({ behavior: 'smooth' });
-  document.getElementById('myNav').style.width = '0%';
-});
-
-
-const news = document.querySelector('#news');
-contact.addEventListener('click', () => {
-  contactSection.scrollIntoView({ behavior: 'smooth' });
   document.getElementById('myNav').style.width = '0%';
 });
